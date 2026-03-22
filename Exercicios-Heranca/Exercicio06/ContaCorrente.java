3a1883cfc79d35117fef73302b32e4fe56b65d0c
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-21 23:15:14 -0300

    Create ContaCorrente.java

---
+++ Exercicios-Heranca/Exercicio06/ContaCorrente.java
@@ -0,0 +1,23 @@
+package main;
+
+public class ContaCorrente extends ContaBancaria{
+    private double limite;
+    
+    public void setLimite(double entraLimite){
+        this.limite = entraLimite;
+    }
+    public double getLimite(){
+        return this.limite;
+    }
+    //override para boas praticas
+    @Override
+    public void sacar(double saiSaldo){
+        //segundo a regra de negocios de bancos o limite de saque é limite + saldo
+        if (saiSaldo>limite+super.verSaldo()){
+            System.out.printf("\nErro, limite ultrapassado...");
+        }
+        else{
+            super.setSaldo(super.verSaldo() - saiSaldo);
+        }
+    }
+}
