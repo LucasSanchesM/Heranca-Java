82d5bd8a0e76373e52aa486fbc690439f695add6
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-22 11:53:38 -0300

    Update Main.java

--- Exercicios-Heranca/Exercicio06/Main.java
+++ Exercicios-Heranca/Exercicio06/Main.java
@@ -5,20 +5,23 @@ public class Main {
     public static void main(String[] args) {
         ContaBancaria novaContaB = new ContaBancaria();
         novaContaB.setNumero(123456);
-        novaContaB.setSaldo(2000);
         novaContaB.setTitular("Jairo");
         
         ContaCorrente novaContaC = new ContaCorrente();
         novaContaC.setNumero(9876);
-        novaContaC.setSaldo(1000);
         novaContaC.setTitular("Alice");
         novaContaC.setLimite(3000);
         
         ContaPoupanca novaContaP = new ContaPoupanca();
         novaContaP.setNumero(12468);
-        novaContaP.setSaldo(1500);
         novaContaP.setTitular("Ramos");
         novaContaP.setRendimento(0.25);
+
+        //coloquei um saldo padrao para testes nas contas
+        novaContaB.depositar(5000);
+        novaContaC.depositar(5000);
+        novaContaP.depositar(5000);
+
         
         //teste conta bancaria
         novaContaB.sacar(1000);
@@ -34,7 +37,7 @@ public class Main {
         
         //teste conta Poupança, irá negar a tentativa de saque superior ao saldo atual
         System.out.printf("\nContaPoupança\nSaldo antes do saque: %.2f", novaContaP.verSaldo());
-        novaContaP.sacar(2000);
+        novaContaP.sacar(20000);
         System.out.printf("\nSaldo depois da tentaiva de saque: %.2f\n", novaContaP.verSaldo());
 
         
